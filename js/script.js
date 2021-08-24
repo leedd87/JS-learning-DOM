2602d81a05ffba2c3463ed0a42d98ffb4bfc1f24
Lee David <leedav87@gmail.com>
2021-08-24 16:15:59 -0300

    interactuando con el DOM

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// console.dir(document)
+
+let reserva1 = document.getElementById('reserva1');
+
+let listaReserva = document.getElementById('listaReserva');
+
+let dom = document.getElementById('dom');
+
+console.dir(reserva1);
+
+console.dir(listaReserva);
+
+console.dir(dom)
