fd74ccf15f51009eae27816fae06863cad0f33e7
Lee David <leedav87@gmail.com>
2021-08-24 18:22:10 -0300

    prompt en js, visualizado en html

--- js/script.js
+++ js/script.js
@@ -2,12 +2,44 @@
 
 let reserva1 = document.getElementById('reserva1');
 
-let listaReserva = document.getElementById('listaReserva');
+// let listaReserva = document.getElementById('listaReserva');
 
 let dom = document.getElementById('dom');
 
 console.dir(reserva1);
 
-console.dir(listaReserva);
+// console.dir(listaReserva);
 
-console.dir(dom)
+// console.dir(dom)
+
+console.dir(reserva1.innerHTML);
+
+// console.dir(listaReserva.innerHTML);
+
+// console.dir(dom.innerHTML)
+
+let reservasTotales = document.getElementsByClassName('reservasTotales');
+
+for (reserva of reservasTotales) {
+    console.log(reserva.innerHTML);
+}
+
+let itemsDeLista = document.getElementsByTagName('li')
+
+for (i = 0; i < itemsDeLista.length; i++) {
+    console.log(itemsDeLista[i].innerText);
+}
+
+//Buscando hoteles de la pagina
+let hoteles = document.getElementById('hoteles');
+
+//agregando un hotel mas a la pagina
+let nuevoHotel = document.createElement('li');
+nuevoHotel.innerText = 'Iberostar';
+
+hoteles.appendChild(nuevoHotel);
+
+let nuevoHotel2 = document.createElement('li')
+nuevoHotel2.innerText = prompt('elegir un hotel');
+
+hoteles.appendChild(nuevoHotel2);
